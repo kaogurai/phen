2a647cc3b459eccd45f15ad647fe022d3e4e0e74
aleclol <50505980+aleclol@users.noreply.github.com>
2021-05-06 16:02:14 -0400

    hardcode rr alias

--- roleutils/reactroles.py
+++ roleutils/reactroles.py
@@ -101,7 +101,7 @@ class ReactRoles(MixinMeta):
         return emoji if isinstance(emoji, str) else str(emoji.id)
 
     @commands.admin_or_permissions(manage_roles=True)
-    @commands.group()
+    @commands.group(aliases=["rr"])
     async def reactrole(self, ctx: commands.Context):
         """Base command for Reaction Role management."""
 
